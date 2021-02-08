0e15cbb9131c65ea9075c368694784c4b13abf34
Sportsguy <mahoward2s@gmail.com>
2021-02-08 11:59:53 -0600

    added index file, added schema file and added code

---
+++ schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+CREATE database employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE department (
+    id INT NOT NULL,
+    name VARCHAR(30) NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL,
+    title VARCHAR(30) NULL,
+    salary DECIMAL NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30) NULL,
+    last_name VARCHAR (30) NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (id)
+);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
