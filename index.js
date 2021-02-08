a798788a8c5bc18a644b7db86616fd8e66b2be6d
Sportsguy <mahoward2s@gmail.com>
2021-02-08 12:03:38 -0600

    added code for index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: 'root',
+
+  // Be sure to update with your own MySQL password!
+  password: '',
+  database: 'top_songsDB',
+});
